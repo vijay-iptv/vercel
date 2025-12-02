e36e4d26995c72a665e1adeee848a2a5f3d87c0a
vijay-iptv <167630289+vijay-iptv@users.noreply.github.com>
2025-12-02 22:47:15 +0530

    Newdhdhh
    Hxbxbd

--- api/result.php
+++ api/result.php
@@ -41,8 +41,8 @@ foreach ($json as $item) {
         $output .= '#KODIPROP:inputstream.adaptive.license_key=' . $item['license_key'] . PHP_EOL;
         $output .= '#EXTVLCOPT:http-user-agent=plaYtv/7.1.3 (Linux;Android 13) ygx/69.1 ExoPlayerLib/824.0' . PHP_EOL;
         
-        $output .= '#EXTHTTP:{"cookie":"'.$jiojsondata[0]['cookie'].'"}'  . PHP_EOL;
-        $output .= 'https://jiotvpllive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$jiojsondata[0]['cookie'].'&xxx=%7Ccookie='.$jiojsondata[0]['cookie'] . PHP_EOL . PHP_EOL;
+        //$output .= '#EXTHTTP:{"cookie":"'.$jiojsondata[0]['cookie'].'"}'  . PHP_EOL;
+        $output .= 'https://jiotvpllive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$jiojsondata[0]['cookie'] . PHP_EOL . PHP_EOL;
         
         //$output .= '#EXTHTTP:{"cookie":"'.$hdnea.'"}'  . PHP_EOL;
         //$output .= 'https://jiotvpllive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$hdnea.'&xxx=%7Ccookie='.$hdnea . PHP_EOL . PHP_EOL;
