910413525fa9127d5133175085582cb8e8f5300f
vijay-iptv <167630289+vijay-iptv@users.noreply.github.com>
2025-12-18 09:55:32 +0530

    Update result.php
    asdfsafdasdf

--- api/result.php
+++ api/result.php
@@ -20,7 +20,7 @@ $json = json_decode(file_get_contents($json_url), true);
 $data = json_decode(file_get_contents($tpjson), true);
 $z5json = json_decode(file_get_contents($z5_json_url), true);
 $jcinemajson = json_decode(file_get_contents($jcinema_json_url), true);
-$cookiedata = ['cookie' => '__hdnea__=st=1766031563~exp=1766117963~acl=/*~hmac=30f716da08f69a44f816c2f3bc4dfb77b9331a45aa6de5bfbaa7d65bf01540fb'];
+$cookiedata = json_decode(file_get_contents($cookie_url), true);
 
 /*if (preg_match('/__hdnea__=[^"}]+/', $jiom3u, $matches)) {
     $hdnea = $matches[0];
@@ -48,11 +48,11 @@ foreach ($json as $item) {
         $output .= '#EXTVLCOPT:http-user-agent=plaYtv/7.1.3 (Linux;Android 13) ygx/69.1 ExoPlayerLib/824.0' . PHP_EOL;
         
         //$output .= '#EXTHTTP:{"cookie":"'.$jiojsondata[0]['cookie'].'"}'  . PHP_EOL;
-        $output .= 'https://jiotvpllive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$cookiedata['cookie'] . PHP_EOL . PHP_EOL;
+        //$output .= 'https://jiotvpllive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$cookiedata['cookie'] . PHP_EOL . PHP_EOL;
         
         //$output .= '#EXTHTTP:{"cookie":"'.$hdnea.'"}'  . PHP_EOL;
         //$output .= 'https://jiotvpllive.cdn.jio.com/bpk-tv/' . $item['bts'] . '/index.mpd?'.$hdnea.'&xxx=%7Ccookie='.$hdnea . PHP_EOL . PHP_EOL;
-        //$output .= 'https://jtvp.8088yyy.workers.dev/bpk-tv/' . $item['bts'] . '/index.mpd|Referer=https://m3u.8088y.fun/'. PHP_EOL . PHP_EOL;
+        $output .= 'https://jtvp.8088yyy.workers.dev/bpk-tv/' . $item['bts'] . '/index.mpd|Referer=https://m3u.8088y.fun/'. PHP_EOL . PHP_EOL;
     }
 }
 // Process M3U lines
