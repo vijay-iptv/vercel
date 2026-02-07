9283c8b490e9782882091538a1ac1edb848f067f
vijay-iptv <167630289+vijay-iptv@users.noreply.github.com>
2026-02-07 11:52:03 +0530

    asdfasdfasdf
    asdfasdfasdf

--- api/result.php
+++ api/result.php
@@ -151,24 +151,18 @@ echo $output . PHP_EOL . PHP_EOL;
 echo $zee5 . PHP_EOL . PHP_EOL;
 echo $jcinema . PHP_EOL . PHP_EOL;
 
-$headers = '|X-Forwarded-For=59.178.74.184|Origin=https://watch.tataplay.com|Referer=https://watch.tataplay.com/"';
+$headers = '&User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)&Origin=https://watch.tataplay.com&Referer=https://watch.tataplay.com/';
 $ctag = 'catchup-type="append" catchup-days="8" catchup-source="&begin={utc}&end={utcend}"';
 $m3uContent = "#EXTM3U x-tvg-url=\"https://avkb.short.gy/epg.xml.gz\"\n#Script by @YGX_WORLD\n\n";
-$m3uContent .= "#KODIPROP:inputstream.adaptive.license_type=clearkey\n";
-$m3uContent .= "#KODIPROP:inputstream.adaptive.manifest_type=dash\n";
-$m3uContent .= "#KODIPROP:inputstream.adaptive.license_key=https://mix.drmlive.net/mix/allowipmpd_key.php?id=chlajabsdk\n";
-$m3uContent .= "#EXTINF:-1 tvg-logo=\"https://i.postimg.cc/d3Yf3PHX/DRMLIVE-Aggressive-Logo.png\" group-title=\"1-Playlist-Activation\",Activate Playlist\n";
-$m3uContent .= "https://mix.drmlive.net/mix/actchaljabsdk.mpd?token=VEZxdmhrQmRpTWxqUEdOYzhTeEFhaGtUc1JzQURjTW9YSE9KWkIyZEZtZ2thbi9qRnpQRUcwN2VFWFlwNkZwc1N3ejZJMTdVSUlnTGVUOWZlYjJ0dVdOeGZsSnhCQ3h4MENIc1dRMitLOWJsODJKdnR4WEw2M0s3ejY2b1NZdGs=\n";
-
 foreach ($data['data']['channels'] as $channel) {
     $id = $channel['id'];
     $name = $channel['name'];
     $logo = $channel['logo_url'];
     $genre = $channel['primaryGenre'] ? 'Tataplay-'.$channel['primaryGenre'] : 'Tataplay-Others';
-    $mpdUrl = 'https://la.drmlive.net/tp/'.$id.'.mpd';
+    $mpdUrl = 'https://salman-bhai-ka.vercel.app/mpd/manifest.mpd?id='.$id.'&apikey=kVVYWJw4XSLYrNqkneSo22lKMLaWukVt';
     
     $m3uContent .= "#KODIPROP:inputstream.adaptive.license_type=clearkey\n";
-    $m3uContent .= "#KODIPROP:inputstream.adaptive.license_key=https://la.drmlive.net/tp/tpck?id=$id\n";
+    $m3uContent .= "#KODIPROP:inputstream.adaptive.license_key=https://salman-bhai-ka.vercel.app/tplay/key/kVVYWJw4XSLYrNqkneSo22lKMLaWukVt/$id\n";
     $m3uContent .= "#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\n";
     $m3uContent .= "#EXTINF:-1 tvg-id=\"ts$id\" $ctag group-title=\"$genre\" tvg-logo=\"https://mediaready.videoready.tv/tatasky-epg/image/fetch/f_auto,fl_lossy,q_auto,h_250,w_250/$logo\",$name\n";
     $m3uContent .= $mpdUrl . $headers . "\n\n";
